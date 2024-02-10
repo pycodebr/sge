6de580c96dcfbc9481371eb76090c2da25fe14b7
Python Code <pycodebr@gmail.com>
2024-02-09 22:22:25 -0300

    Add category products filter and fix align footer

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from app import metrics
+from categories.models import Category
 from . import models, forms
 
 
@@ -14,11 +15,14 @@ class ProductListView(ListView):
         queryset = super().get_queryset()
         title = self.request.GET.get('title')
         serie_number = self.request.GET.get('serie_number')
+        category = self.request.GET.get('category')
 
         if title:
             queryset = queryset.filter(title__icontains=title)
         if serie_number:
             queryset = queryset.filter(serie_number__icontains=serie_number)
+        if category:
+            queryset = queryset.filter(category_id=category)
 
         return queryset
 
@@ -26,6 +30,7 @@ class ProductListView(ListView):
         context = super().get_context_data(**kwargs)
         context['product_metrics'] = metrics.get_product_metrics()
         context['sales_metrics'] = metrics.get_sales_metrics()
+        context['categories'] = Category.objects.all()
         return context
 
 
