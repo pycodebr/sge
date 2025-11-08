482b94dea791784dfe6257323774d1a2f9f14b07
Fernando Sousa <fernando.sousa@brlink.com.br>
2025-11-08 13:04:54 -0300

    Fornecedor OK

--- application/suppliers/models.py
+++ application/suppliers/models.py
@@ -9,7 +9,7 @@ class Supplier(models.Model):
     document_id = models.CharField(max_length=15, blank=True)
     contact = models.CharField(max_length=50, blank=True)
     contact_phone = models.CharField(max_length=20, blank=True)
-    address = models.CharField(max_length=300, blank=True)
+    address = models.CharField(max_length=50, blank=True)
     city = models.CharField(max_length=30, blank=True)
     state = models.CharField(max_length=2, blank=True)
     zip_code = models.CharField(max_length=10, blank=True)
